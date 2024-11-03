b148de02a0b828fe0cf339cd8218cd4aac013d8f
Luis Vasquez <luislve1796@gmail.com>
2024-11-03 17:14:07 -0500

    Second try enabling ssh

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -115,6 +115,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  programs.ssh.startAgent = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
